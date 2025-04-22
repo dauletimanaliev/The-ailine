bc20281d9a67c7eb641819392231458234e7f98a
Daulet <126412542+Bastau-Bot@users.noreply.github.com>
2025-04-22 12:10:28 +0500

    gen1

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class AilineApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Ailin',
+      title: '',
       home: MapHomeScreen(),
     );
   }
